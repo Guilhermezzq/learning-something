10a0481b3aca8aa5b62c9794e6828f34d7e9d228
Guilherme <119014317+Guilhermezzq@users.noreply.github.com>
2024-06-06 16:10:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+import logoThreeJs from './assets/threejs.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+// Produz um índice aleatório que gera as diferentes palavras possíveis de forma aleatória
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+
+  
+}
+
+
+function Header() {
+  const [descriptionIndex, setDescriptionIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDescriptionIndex(getRandomInt(2));
+    }, 2000);
+
+    return () => clearInterval(intervalId);
+  }, []); // O segundo argumento vazio faz com que o useEffect seja executado apenas uma vez, após a montagem do componente
+
+
+
+
+  return (
+    <header>
+      <img src={logoThreeJs} alt="Stylized atom" />
+      <h1>Three Js</h1>
+      <p>
+      {reactDescriptions[descriptionIndex]} Three.js principles for crafting immersive 3D experiences in any web application you aspire to develop
+      </p>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
