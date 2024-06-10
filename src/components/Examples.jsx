ce04f50cb94ed5cb3332c6780cea01340616dd02
Guilherme <119014317+Guilhermezzq@users.noreply.github.com>
2024-06-10 17:23:17 -0300

    update

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import TabButton from "./TabButton";
 
 
@@ -5,20 +6,24 @@ import TabButton from "./TabButton";
 
 const Examples = () => {
 
-    function handleSelect() {
-        console.log('selected!')
+  const [ selectedTopic, setSelectedTopic ] = useState("Please click a button")
+
+  function handleSelect(selectedButton) {
+      // selectedButton => 'components' , 'jsx' 'props' , 'state'
+      setSelectedTopic(selectedButton);
+      console.log(selectedTopic);
     }
 
   return (
     <section id="examples">
         <h2>Examples</h2>
         <menu>
-             <TabButton onSelect={handleSelect} >Animation</TabButton>
-             <TabButton onSelect={handleSelect}>Camera</TabButton>
-             <TabButton onSelect={handleSelect} >Materials</TabButton>
-             <TabButton onSelect={handleSelect}   >Object3D</TabButton>
+             <TabButton onSelect={ () => handleSelect('animation')} >Animation</TabButton>
+             <TabButton onSelect={ () => handleSelect('camera')}>Camera</TabButton>
+             <TabButton onSelect={ () => handleSelect('materials')} >Materials</TabButton>
+             <TabButton onSelect={ () => handleSelect('object3d')}   >Object3D</TabButton>
         </menu>
-        
+        {selectedTopic}
     </section>
   )
 }
